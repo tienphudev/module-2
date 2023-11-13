2421e719321814a800e4e83ce96aa2f73e8130f2
tienphudev <phuqtp@gmail.com>
2023-11-13 10:23:26 +0700

    studentArray

---
+++ MaxInArray/src/MaxinArray.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MaxinArray {
+    public static void main(String[] args) {
+
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size should not exceed 20");
+        } while (size > 20);
+
+    }
+}
